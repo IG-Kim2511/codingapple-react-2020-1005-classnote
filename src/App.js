a12506561e13955292d2a992d2a766e6a5dd32ee
IG-Kim <iikim2511@gmail.com>
2020-10-03 12:18:11 -0400

    03 JSX를 이용해 HTML 페이지 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var data = "안녕하세요";
+
   return (
     <div className="App">
+      <div className="black-nav">
+        <div>03 JSX를 이용해 HTML 페이지 제작</div>
+        <div>{data}</div>
+      </div>
 
+      <div className="black-nav">
+        <div>03 JSX를 이용해 HTML 페이지 제작</div>
+        <div className={data}>{data}</div>
+      </div>
+
+      <div style={{ color: "blue", fontSize: "30px" }}>
+        {" "}
+        03 JSX를 이용해 HTML 페이지 제작{" "}
+      </div>
     </div>
   );
 }
